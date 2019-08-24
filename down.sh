397a65e40fe47d6c50b74e738acd71ea198c3632
max.weis <max.weis@openknowledge.de>
2019-08-24 10:38:10 +0200

    move to parent dir

--- down.sh
+++
@@ -1,9 +0,0 @@
-
-go get github.com/mdempsky/gocode
-go get github.com/ramya-rao-a/go-outline
-go get github.com/acroca/go-symbols
-go get golang.org/x/tools/cmd/guru
-go get golang.org/x/tools/cmd/gorename
-go get github.com/stamblerre/gocode
-go get github.com/sqs/goreturns
-go get golang.org/x/lint/golint
